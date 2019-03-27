9b5e6e23ead77fee87aa1a27f6d5efc97d09c4d3
TRileySchwarz <timothy@vanbex.com>
2019-03-27 16:40:41 -0700

    Edited typo

--- .solcover.js
+++ .solcover.js
@@ -4,7 +4,6 @@ module.exports = {
     norpc: true,
     skipFiles: [
         // Comment out which files you want to see the coverage for.
-        // Before running coverage, make sure to copy the node modules contracts folder into 'openZeppelinContracts'
         'Migrations.sol'
         //'mocks'
     ]
